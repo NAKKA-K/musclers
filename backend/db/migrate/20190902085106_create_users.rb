f44552f3e561fc5983bf1a779cafaae1c124cc57
kageyasai2 <kd1244160@st.kobedenshi.ac.jp>
2019-09-02 19:14:03 +0900

    [change]column & [delete]schema.rb

--- backend/db/migrate/20190902085106_create_users.rb
+++ backend/db/migrate/20190902085106_create_users.rb
@@ -2,8 +2,6 @@ class CreateUsers < ActiveRecord::Migration[5.2]
   def change
     create_table :users do |t|
       t.string :nickname
-      t.string :email
-      t.string :password
       t.string :thumbnail
       t.text :description
       t.integer :age
