0b5735b85476c85b679c0396a3cb925a32354f4a
kageyasai2 <kd1244160@st.kobedenshi.ac.jp>
2019-09-02 18:41:18 +0900

    [add]user model

---
+++ backend/db/migrate/20190902085106_create_users.rb
@@ -0,0 +1,21 @@
+class CreateUsers < ActiveRecord::Migration[5.2]
+  def change
+    create_table :users do |t|
+      t.string :nickname
+      t.string :email
+      t.string :password
+      t.string :thumbnail
+      t.text :description
+      t.integer :age
+      t.integer :gender
+      t.integer :height
+      t.integer :weight
+      t.integer :figure
+      t.integer :muscle_mass
+      t.integer :body_fat_percentage
+      t.integer :seriousness
+
+      t.timestamps
+    end
+  end
+end
