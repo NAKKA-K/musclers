061c4dff5569a4d99bb10d93cc615793648e88d7
various3211workd <kd1229568@st.kobedenshi.ac.jp>
2019-09-02 18:18:24 +0900

    [Fix] Can use Eslint

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -8,17 +8,16 @@ module.exports = {
     parser: 'babel-eslint'
   },
   extends: [
-    "eslint:recommended",
-    "plugin:vue/recommended",
-    "plugin:prettier/recommended"
+    '@nuxtjs',
+    'prettier',
+    'prettier/vue',
+    'plugin:prettier/recommended',
+    'plugin:nuxt/recommended'
   ],
   plugins: [
-    'vue'
+    'prettier'
   ],
+  // add your custom rules here
   rules: {
-    "semi": [2, "never"],
-    "no-console": "off",
-    "vue/max-attributes-per-line": "off",
-    "prettier/prettier": ["error", { "semi": false }]
   }
 }
