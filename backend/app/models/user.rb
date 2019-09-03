0dc962eb45ca3be1b82135f6baadbc4a13ed9cc7
kageyasai2 <kd1244160@st.kobedenshi.ac.jp>
2019-09-03 11:18:36 +0900

    [add]enum japanese Correspondence

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -1,13 +1,13 @@
 class User < ApplicationRecord
-  enum gender:  { "未設定": 0, "男性": 1, "女性": 2, "その他": 3}, _prefix: true
+  enum gender:  { Not_set: 0, Man: 1, Woman: 2, Other: 3}, _prefix: true
   enum figure:  {
-                  "未設定": 0,
-                  "痩せ型筋肉質": 1,
-                  "普通筋肉質": 5,
-                  "肥満型筋肉質": 10,
-                  "痩せ型": 15,
-                  "普通": 20,
-                  "肥満型":25,
-                  "その他": 99
+                  Not_set: 0,
+                  Skinny_muscle: 1,
+                  Normal_muscle: 5,
+                  Obese_muscle: 10,
+                  Skinny: 15,
+                  Normal: 20,
+                  Obese:25,
+                  Other: 99
                 }, _prefix: true
 end
