5f612c2d06fd5093e714e20ef5213c5fa2066bc6
Raccoo <kd1196317@st.kobedenshi.ac.jp>
2019-09-05 13:44:18 +0900

    Update default.vue

--- frontend/layouts/default.vue
+++ frontend/layouts/default.vue
@@ -1,93 +1,74 @@
 <template>
-  <v-app dark>
-    <v-navigation-drawer
-      v-model="drawer"
-      :mini-variant="miniVariant"
-      :clipped="clipped"
-      fixed
-      app
-    >
-      <v-list>
-        <v-list-tile
-          v-for="(item, i) in items"
-          :key="i"
-          :to="item.to"
-          router
-          exact
-        >
-          <v-list-tile-action>
-            <v-icon>{{ item.icon }}</v-icon>
-          </v-list-tile-action>
-          <v-list-tile-content>
-            <v-list-tile-title v-text="item.title" />
-          </v-list-tile-content>
-        </v-list-tile>
-      </v-list>
-    </v-navigation-drawer>
-    <v-toolbar :clipped-left="clipped" fixed app>
-      <v-toolbar-side-icon @click="drawer = !drawer" />
-      <v-btn icon @click.stop="miniVariant = !miniVariant">
-        <v-icon>{{ `chevron_${miniVariant ? 'right' : 'left'}` }}</v-icon>
-      </v-btn>
-      <v-btn icon @click.stop="clipped = !clipped">
-        <v-icon>web</v-icon>
-      </v-btn>
-      <v-btn icon @click.stop="fixed = !fixed">
-        <v-icon>remove</v-icon>
-      </v-btn>
-      <v-toolbar-title v-text="title" />
-      <v-spacer />
-      <v-btn icon @click.stop="rightDrawer = !rightDrawer">
-        <v-icon>menu</v-icon>
-      </v-btn>
+  <div id="app">
+    <v-app id="inspire">
+      <v-toolbar>
+        <div>
+          <v-toolbar-title>Muscler's</v-toolbar-title>
+        </div>
+        <div>
+          <v-textarea
+          label="ユーザー検索"
+          prepend-inner-icon="search"
+          rows="1"
+          />
+        </div>
+        <v-toolbar-items class="hidden-sm-and-down">
+          <div>
+            <v-select
+            item-text="title"
+            :items="group"
+            label="グループ"
+            return-object
+            />
+          </div>
+        </v-toolbar-items>
+        <v-spacer></v-spacer>
+        <a>
+          <v-badge style="margin:25px;">
+            <template v-slot:badge>0</template>
+          </v-badge>
+        </a>
+        <v-menu offset-y>
+          <template v-slot:activator="{ on }">
+            <i v-on="on" class="material-icons" btn>account_circle</i>
+          </template>
+          <v-list>
+            <v-list-item
+            v-for="(item, index) in items"
+            :key="index"
+            @click=""
+            size="18">
+              <v-list-item-title>{{ item.title }}</v-list-item-title><br>
+            </v-list-item>
+          </v-list>
+        </v-menu>
     </v-toolbar>
-    <v-content>
-      <v-container>
-        <nuxt />
-      </v-container>
-    </v-content>
-    <v-navigation-drawer v-model="rightDrawer" :right="right" temporary fixed>
-      <v-list>
-        <v-list-tile @click.native="right = !right">
-          <v-list-tile-action>
-            <v-icon light>
-              compare_arrows
-            </v-icon>
-          </v-list-tile-action>
-          <v-list-tile-title>Switch drawer (click me)</v-list-tile-title>
-        </v-list-tile>
-      </v-list>
-    </v-navigation-drawer>
-    <v-footer :fixed="fixed" app>
-      <span>&copy; 2019</span>
-    </v-footer>
-  </v-app>
+   </v-app>
+   <v-footer absolute class="font-weight-medium">
+    <v-col class="text-center" cols="12">
+       プライバシーポリシー・利用規約<br>お問い合わせ  
+    </v-col>
+   </v-footer>
+  </div>
 </template>
-
 <script>
-export default {
-  data() {
-    return {
-      clipped: false,
-      drawer: false,
-      fixed: false,
+  export default {
+    data: () => ({
       items: [
-        {
-          icon: 'apps',
-          title: 'Welcome',
-          to: '/'
-        },
-        {
-          icon: 'bubble_chart',
-          title: 'Inspire',
-          to: '/inspire'
-        }
+        { title: 'マイページ' },
+        { title: 'おすすめユーザー' },
+        { title: 'メッセージ' },
+        { title: 'グループ' },
+        { title: '検索' },
+        { title: 'ログアウト' },
       ],
-      miniVariant: false,
-      right: true,
-      rightDrawer: false,
-      title: 'Vuetify.js'
-    }
+      group:[
+        { title: 'グループ検索' },
+        { title: 'グループ作成' },
+        { title: 'グループ一覧' },
+        { title: '参加グループ' },
+
+      ]
+    }),
   }
-}
 </script>
