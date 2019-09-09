cd6ecc66132c68152d5896a725db03593cad2c5b
Raccoo <kd1196317@st.kobedenshi.ac.jp>
2019-09-09 17:36:15 +0900

    [Fix]Code format

--- frontend/layouts/TheHeader.vue
+++ frontend/layouts/TheHeader.vue
@@ -7,63 +7,61 @@
         </div>
         <div>
           <v-textarea
-          label="ユーザー検索"
-          prepend-inner-icon="search"
-          rows="1"
+            label="ユーザー検索"
+            prepend-inner-icon="search"
+            rows="1"
           />
         </div>
         <v-toolbar-items class="hidden-sm-and-down">
           <div>
             <v-select
-            item-text="title"
-            :items="group"
-            label="グループ"
-            return-object
+              item-text="title"
+              :items="group"
+              label="グループ"
+              return-object
             />
           </div>
         </v-toolbar-items>
         <v-spacer></v-spacer>
         <a>
           <v-badge style="margin:25px;">
-            <template v-slot:badge>0</template>
+            <template v-slot:badge
+              >0</template
+            >
           </v-badge>
         </a>
         <v-menu offset-y>
           <template v-slot:activator="{ on }">
-            <i v-on="on" class="material-icons" btn>account_circle</i>
+            <i class="material-icons" btn>account_circle</i>
           </template>
           <v-list>
-            <v-list-item
-            v-for="(item, index) in items"
-            :key="index"
-            @click=""
-            size="18">
-              <v-list-item-title>{{ item.title }}</v-list-item-title><br>
+            <v-list-item v-for="(item, i) in items" :key="i">
+              <v-list-item-title>{{ item.title }}</v-list-item-title
+              ><br />
             </v-list-item>
           </v-list>
         </v-menu>
-    </v-toolbar>
-   </v-app>
+      </v-toolbar>
+    </v-app>
   </div>
 </template>
 <script>
-  export default {
-    data: () => ({
-      items: [
-        { title: 'マイページ' },
-        { title: 'おすすめユーザー' },
-        { title: 'メッセージ' },
-        { title: 'グループ' },
-        { title: '検索' },
-        { title: 'ログアウト' },
-      ],
-      group:[
-        { title: 'グループ検索' },
-        { title: 'グループ作成' },
-        { title: 'グループ一覧' },
-        { title: '参加グループ' },
-
-      ]
-    }),
-  }
+export default {
+  data: () => ({
+    items: [
+      { title: 'マイページ' },
+      { title: 'おすすめユーザー' },
+      { title: 'メッセージ' },
+      { title: 'グループ' },
+      { title: '検索' },
+      { title: 'ログアウト' }
+    ],
+    group: [
+      { title: 'グループ検索' },
+      { title: 'グループ作成' },
+      { title: 'グループ一覧' },
+      { title: '参加グループ' }
+    ]
+  })
+}
 </script>
--- frontend/layouts/default.vue
+++ frontend/layouts/default.vue
@@ -1,16 +1,16 @@
 <template>
   <div id="app">
-  <TheHeader/>
-  <TheFooter/>
-</div>
+    <TheHeader />
+    <TheFooter />
+  </div>
 </template>
 <script>
 import TheHeader from './TheHeader.vue'
 import TheFooter from './TheFooter.vue'
 
-export default{
-  name:'app',
-  components:{
+export default {
+  name: 'App',
+  components: {
     TheHeader,
     TheFooter
   }
