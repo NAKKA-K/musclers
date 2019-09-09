5f9730846c824b476fe82c265732a82082e298b4
Raccoo <kd1196317@st.kobedenshi.ac.jp>
2019-09-09 17:36:07 +0900

    [Remove]header.vue,footer.vue

--- frontend/layouts/footer.vue
+++
@@ -1,9 +0,0 @@
-<template>
-	<div id="app">
-   <v-footer absolute class="font-weight-medium">
-    <v-col class="text-center" cols="12">
-       プライバシーポリシー・利用規約<br>お問い合わせ  
-    </v-col>
-   </v-footer>
-</div>
-</template>
