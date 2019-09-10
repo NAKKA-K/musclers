012182a1484c48ddb1c403b314df5ca43cfc8f4c
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2019-09-10 23:18:30 +0900

    Add sign in route

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,19 +1,17 @@
 Rails.application.routes.draw do
   namespace 'api', defaults: { format: :json } do
-    mount_devise_token_auth_for 'User', at: 'auth', controllers: {
-      omniauth_callbacks: 'api/omni_auth'
-    }
-    get '/my_callback', to: 'omni_auth#facebook'
-  end
-  # devise_for :users , controllers: { omniauth_callbacks: 'api/omni_auth' }
+    namespace 'auth' do
+      post '/sign_in', to: 'oauth#facebook'
+    end
 
-  namespace 'api' do
     resources :users do
       collection do
         get :search
       end
     end
+
   end
+
   get 'hoge/show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
