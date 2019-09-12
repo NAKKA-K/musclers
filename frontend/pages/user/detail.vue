5058b145d653bbb8fbb657868f4e4aa462ff31e3
kageyasai2 <kd1244160@st.kobedenshi.ac.jp>
2019-09-12 13:38:55 +0900

    [WIP]detail page

--- frontend/pages/user/detail.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <h1>heloo worldnn</h1>
-</template>
-
-<script>
-export default {
-  components: {}
-}
-</script>
--- frontend/pages/user/detail/_id.vue
+++ frontend/pages/user/detail/_id.vue
@@ -7,30 +7,69 @@
         </div>
         <div v-else>
           <v-card>
-            <v-img src="https://cdn.vuetifyjs.com/images/john.png"></v-img>
-            <v-card-text>
-              <div class="title text--primary">
-                年齢：{{ user_detail_data.age }}
-              </div>
-              <div class="title text--primary">
-                性別：{{ user_detail_data.gender }}
-              </div>
-              <div class="title text--primary">
-                身長：{{ user_detail_data.height }}
-              </div>
-              <div class="title text--primary">
-                体重：{{ user_detail_data.weight }}
-              </div>
-              <div class="title text--primary">
-                体型：{{ user_detail_data.figure }}
-              </div>
-              <div class="title text--primary">
-                筋肉量：
-              </div>
-              <div class="title text--primary">体脂肪率：</div>
-              <div class="title text--primary">
-                本気度：{{ user_detail_data.seriousness }}
-              </div>
+            <v-img
+              height="20vh"
+              src="https://cdn.vuetifyjs.com/images/john.png"
+            ></v-img>
+            <v-list-item>
+              <v-list-item-content>
+                <v-list-item-title class="headline"
+                  >Our Changing Planet</v-list-item-title
+                >
+                <v-list-item-subtitle>by Kurt Wagner</v-list-item-subtitle>
+              </v-list-item-content>
+            </v-list-item>
+            <v-card-title>YYAMADA TARO</v-card-title>
+            <v-card-text class="detail-content">
+              <ol>
+                <li v-if="user_detail_data.age" class="title text--primary">
+                  年齢：{{ user_detail_data.age }}
+                </li>
+                <li v-else class="title text--primary">
+                  年齢：未設定
+                </li>
+                <li v-if="user_detail_data.gender" class="title text--primary">
+                  性別：{{ user_detail_data.gender }}
+                </li>
+                <li v-else class="title text--primary">
+                  性別：未設定
+                </li>
+                <li v-if="user_detail_data.height" class="title text--primary">
+                  身長：{{ user_detail_data.height }}
+                </li>
+                <li v-else class="title text--primary">
+                  身長：未設定
+                </li>
+                <li v-if="user_detail_data.weight" class="title text--primary">
+                  体重：{{ user_detail_data.weight }}
+                </li>
+                <li v-else class="title text--primary">
+                  体重：未設定
+                </li>
+                <li v-if="user_detail_data.figure" class="title text--primary">
+                  体型：{{ user_detail_data.figure }}
+                </li>
+                <li v-else class="title text--primary">
+                  体型：未設定
+                </li>
+                <!--
+                TODO: 筋肉量と体脂肪率の項目は
+                ユーザ詳細APIのレスポンスに筋肉量と体脂肪率のデータが含まれてから処理を書く
+              -->
+                <li class="title text--primary">
+                  筋肉量：未設定
+                </li>
+                <li class="title text--primary">体脂肪率：未設定</li>
+                <li
+                  v-if="user_detail_data.seriousness"
+                  class="title text--primary"
+                >
+                  本気度：{{ user_detail_data.seriousness }}
+                </li>
+                <li v-else class="title text--primary">
+                  本気度：未設定
+                </li>
+              </ol>
             </v-card-text>
           </v-card>
         </div>
