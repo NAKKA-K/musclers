3a39c2ca275cba61e31ce5e3a30dd13daaf28f8b
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2019-09-12 11:57:26 +0900

    Remove v-app tag

--- frontend/layouts/TheFooter.vue
+++ frontend/layouts/TheFooter.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="app">
+  <div>
     <v-footer absolute class="font-weight-medium">
       <v-col class="text-center" cols="12">
         プライバシーポリシー・利用規約<br />お問い合わせ
--- frontend/layouts/TheHeader.vue
+++ frontend/layouts/TheHeader.vue
@@ -1,54 +1,52 @@
 <template>
-  <div id="app">
-    <v-app id="inspire">
-      <v-toolbar flat :color="'#ffac12'">
-        <div>
-          <v-toolbar-title>Muscler's</v-toolbar-title>
-        </div>
-        <div class="search-bar">
-          <v-text-field
-            label="ユーザー検索"
-            hide-details
-            prepend-icon="search"
-            single-line
+  <div>
+    <v-toolbar flat :color="'#ffac12'">
+      <div>
+        <v-toolbar-title>Muscler's</v-toolbar-title>
+      </div>
+      <div class="search-bar">
+        <v-text-field
+          label="ユーザー検索"
+          hide-details
+          prepend-icon="search"
+          single-line
+        />
+      </div>
+      <v-toolbar-items class="hidden-sm-and-down">
+        <div class="select-size">
+          <v-select
+            item-text="title"
+            :items="group"
+            label="グループ"
+            return-object
           />
         </div>
-        <v-toolbar-items class="hidden-sm-and-down">
-          <div class="select-size">
-            <v-select
-              item-text="title"
-              :items="group"
-              label="グループ"
-              return-object
-            />
-          </div>
-        </v-toolbar-items>
-        <v-spacer></v-spacer>
-        <v-badge color="red" overlap class="badge-position">
-          <i class="material-icons">notifications</i>
-          <!--TODO:通知きた時用の分岐<v-if>-->
-          <template v-slot:badge>
-            <span>1</span>
-          </template>
-        </v-badge>
-        <v-menu>
-          <template v-slot:activator="{ on }">
-            <v-btn icon v-on="on">
-              <i class="material-icons large-size">account_circle</i>
-            </v-btn>
-          </template>
+      </v-toolbar-items>
+      <v-spacer></v-spacer>
+      <v-badge color="red" overlap class="badge-position">
+        <i class="material-icons">notifications</i>
+        <!--TODO:通知きた時用の分岐<v-if>-->
+        <template v-slot:badge>
+          <span>1</span>
+        </template>
+      </v-badge>
+      <v-menu>
+        <template v-slot:activator="{ on }">
+          <v-btn icon v-on="on">
+            <i class="material-icons large-size">account_circle</i>
+          </v-btn>
+        </template>
 
-          <v-list>
-            <v-list-item v-for="(item, i) in items" :key="i" @click="() => {}">
-              <v-list-item-title>
-                {{ item.title }}
-              </v-list-item-title>
-              <br />
-            </v-list-item>
-          </v-list>
-        </v-menu>
-      </v-toolbar>
-    </v-app>
+        <v-list>
+          <v-list-item v-for="(item, i) in items" :key="i" @click="() => {}">
+            <v-list-item-title>
+              {{ item.title }}
+            </v-list-item-title>
+            <br />
+          </v-list-item>
+        </v-list>
+      </v-menu>
+    </v-toolbar>
   </div>
 </template>
 
