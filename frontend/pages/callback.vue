919485e0f80088a0b041b31ee0a3d00eecaca8d4
kageyasai2 <kd1244160@st.kobedenshi.ac.jp>
2019-10-04 09:52:31 +0900

    [delete]debug message

--- frontend/pages/callback.vue
+++ frontend/pages/callback.vue
@@ -18,20 +18,14 @@ export default {
     }
 
     try {
-      console.log('debug1')
       const res = await this.$axios.$post('/api/auth/sign_in', postData)
-      console.log('debug2')
       await this.$store.dispatch('auth/setCurrentUser', { user: res.data })
-      console.log('debug3')
-      if (res.data.email) {
-        console.log('debug4')
+      if (res.data.email !== '') {
         this.$router.push('/users')
       } else {
-        console.log('debug5')
         this.$router.push('/register') // TODO: Email等の入力画面に遷移させる
       }
     } catch (err) {
-      console.log('debug6')
       console.error(err)
     }
   }
