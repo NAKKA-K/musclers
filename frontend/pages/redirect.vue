44f95e8fa043701118350eeb2ee22dbde0a7db39
kageyasai2 <kd1244160@st.kobedenshi.ac.jp>
2019-10-07 12:30:59 +0900

    [fix]page redirect bug & login bug

---
+++ frontend/pages/redirect.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>ログインしています......</div>
+</template>
+
+<script>
+export default {
+  async mounted() {
+    const state = this.$auth.$state
+    if (!state.loggedIn) {
+      console.error('認証できませんでした')
+      return this.$router.push('/auth/login')
+    } else {
+      const postData = {
+        uid: state.user.id,
+        provider: state.strategy,
+        email: state.user.email
+      }
+
+      try {
+        const res = await this.$axios.$post('/api/auth/sign_in', postData)
+        this.$store.dispatch('auth/setCurrentUser', { user: res.data })
+        if (res.data.email !== '') {
+          this.$router.push('/users')
+        } else {
+          this.$router.push('/register') // TODO: Email等の入力画面に遷移させる
+        }
+      } catch (err) {
+        console.error(err)
+      }
+    }
+  }
+}
+</script>
