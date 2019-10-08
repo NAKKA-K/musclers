343c24b5dfb0c4355d00848a5aa65297ba84f598
kageyasai2 <kd1244160@st.kobedenshi.ac.jp>
2019-10-08 12:08:05 +0900

    [add]index page

---
+++ frontend/layouts/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-app id="inspire">
+    <div id="app">
+      <TheHeader />
+      <div class="container-fluid">
+        <nuxt />
+      </div>
+      <TheFooter />
+    </div>
+  </v-app>
+</template>
+
+<script>
+import TheHeader from './TheHeader.vue'
+import TheFooter from './TheFooter.vue'
+
+export default {
+  name: 'App',
+  components: {
+    TheHeader,
+    TheFooter
+  }
+}
+</script>
