e9602556ef937b3b6b18faed4e6f196b429d2595
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2019-10-09 12:07:28 +0900

    Add v-app tag

--- frontend/layouts/login.vue
+++ frontend/layouts/login.vue
@@ -1,11 +1,13 @@
 <template>
-  <div id="app">
-    <v-app id="inspire" style="background-color: #ffb74d;">
-      <TheHeader />
-      <nuxt />
-      <TheFooter />
-    </v-app>
-  </div>
+  <v-app id="inspire">
+    <div id="app">
+      <v-app id="inspire" style="background-color: #ffb74d;">
+        <TheHeader />
+        <nuxt />
+        <TheFooter />
+      </v-app>
+    </div>
+  </v-app>
 </template>
 
 <script>
