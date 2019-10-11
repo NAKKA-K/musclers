70f8f82b6172b2d73e8738f1c1f597af0641e707
various3211workd <kd1229568@st.kobedenshi.ac.jp>
2019-10-11 12:09:22 +0900

    [wip] layout...

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,3 +1,4 @@
+<!--
 <template>
   <div>
     <div class="top-wrapper">
@@ -93,3 +94,51 @@ export default {
 <style>
 @import '@/assets/css/index.css';
 </style>
+-->
+<!-- debug用画面 -->
+<template>
+  <div>
+    <v-layout justify-center>
+      <v-form>
+        <div class="container">
+          <v-col>
+            <v-row>
+              <v-text-field
+                flat
+                solo-inverted
+                append-icon="search"
+                label="キーワードで検索"
+                hide-details
+              >
+              </v-text-field>
+            </v-row>
+            <v-row>
+              <v-checkbox
+                v-model="checkbox"
+                label="Do you agree?"
+                required
+              ></v-checkbox>
+              <v-checkbox
+                v-model="checkbox"
+                label="Do you agree?"
+                required
+              ></v-checkbox>
+              <v-checkbox
+                v-model="checkbox"
+                label="Do you agree?"
+                required
+              ></v-checkbox>
+            </v-row>
+            <v-row>
+              <v-select label="Item" required></v-select>
+            </v-row>
+            <v-btn color="info">
+              検索する
+            </v-btn>
+          </v-col>
+        </div>
+      </v-form>
+    </v-layout>
+  </div>
+</template>
+<script></script>
