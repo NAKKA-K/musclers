3a95a711f5ff23c099e34731d2a6991f69daaf39
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2019-10-16 16:34:33 +0900

    Remove debug log

--- frontend/plugins/queryParamsUpdater.js
+++ frontend/plugins/queryParamsUpdater.js
@@ -1,9 +1,5 @@
 export default ({ app, store }) => {
   app.router.beforeEach((to, from, next) => {
-    console.log('------')
-    console.log(store.getters['users/shapedSearchParameters'])
-    console.log(to.query)
-    console.log('------')
     store.commit('users/setFilteredSearchParameters', { queries: to.query })
     next({ query: store.getters['users/shapedSearchParameters'] })
   })
