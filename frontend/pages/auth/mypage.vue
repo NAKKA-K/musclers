86c02d4d181ed942400dbd55ca857f4e6b1a40b0
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2019-10-30 16:57:04 +0900

    Add mypage

--- frontend/pages/auth/mypage.vue
+++ frontend/pages/auth/mypage.vue
@@ -2,56 +2,205 @@
   <div>
     <h2>マイページ</h2>
 
-    <v-text-field v-model="nickname" label="ニックネーム"></v-text-field>
+    <v-layout class="justify-end">
+      <v-btn class="my-4" tile outlined color="success" @click="enableEdit">
+        <v-icon small>edit</v-icon>編集
+      </v-btn>
+    </v-layout>
 
-    <v-text-field v-model="age" label="年齢" disabled></v-text-field>
+    <v-text-field
+      v-model="nickname"
+      label="ニックネーム"
+      :disabled="disabled"
+    ></v-text-field>
 
-    <v-text-field v-model="gender" label="性別" disabled></v-text-field>
+    <v-text-field
+      v-model="age"
+      label="年齢"
+      :disabled="disabled"
+    ></v-text-field>
 
-    <v-text-field v-model="height" label="身長" disabled></v-text-field>
-
-    <v-text-field v-model="weight" label="体重" disabled></v-text-field>
-
-    <v-radio-group v-model="seriousness" row disabled>
-      <v-radio label="ガチ" value="serious"></v-radio>
-      <v-radio label="エンジョイ" value="enjoy"></v-radio>
-      <v-radio label="未設定" value="none"></v-radio>
+    <v-radio-group v-model="gender" label="性別" :disabled="disabled">
+      <v-radio
+        v-for="hash in gendersArray"
+        :key="hash.value"
+        :label="hash.label"
+        :value="hash.value"
+      ></v-radio>
     </v-radio-group>
 
-    <p>-{{ currentUser }}-</p>
-    <p>-{{ user }}-</p>
+    <v-text-field
+      v-model="height"
+      label="身長"
+      :disabled="disabled"
+    ></v-text-field>
+
+    <v-text-field
+      v-model="weight"
+      label="体重"
+      :disabled="disabled"
+    ></v-text-field>
+
+    <v-radio-group v-model="seriousness" label="本気度" :disabled="disabled">
+      <v-radio
+        v-for="hash in seriousnessArray"
+        :key="hash.value"
+        :label="hash.label"
+        :value="hash.value"
+      ></v-radio>
+    </v-radio-group>
+
+    <v-select
+      v-model="figure"
+      :items="figuresArray"
+      item-text="label"
+      item-value="value"
+      label="体系"
+      :disabled="disabled"
+    ></v-select>
+
+    <v-text-field
+      v-model="muscle_mass"
+      type="number"
+      label="筋肉量"
+      :disabled="disabled"
+    ></v-text-field>
+
+    <v-text-field
+      v-model="body_fat_percentage"
+      type="number"
+      label="体脂肪率"
+      :disabled="disabled"
+    ></v-text-field>
   </div>
 </template>
 
 <script>
-import { mapGetters } from 'vuex'
-
 export default {
   middleware: 'auth',
   data: () => ({
-    user: {}
+    disabled: true,
+    user: {},
+    seriousnessArray: [
+      { label: '未設定', value: 0 },
+      { label: 'エンジョイ', value: 2 },
+      { label: 'ガチ', value: 1 }
+    ],
+    gendersArray: [
+      { label: '未設定', value: 0 },
+      { label: '男性', value: 1 },
+      { label: '女性', value: 2 },
+      { label: 'その他', value: 3 }
+    ],
+    figuresArray: [
+      { label: '未設定', value: 0 },
+      { label: '痩せ型筋肉質', value: 1 },
+      { label: '普通筋肉質', value: 5 },
+      { label: '肥満型筋肉質', value: 10 },
+      { label: '痩せ型', value: 15 },
+      { label: '普通', value: 20 },
+      { label: '肥満型', value: 25 },
+      { label: 'その他', value: 99 }
+    ]
   }),
 
   computed: {
-    ...mapGetters('auth', ['currentUser']),
     nickname: {
       get: (vm) => (vm.user ? vm.user.nickname : null),
       set(val) {
-        this.user.nickname = val
+        this.setUserPartial('nickname', val)
       }
     },
-    age: (vm) => (vm.user ? vm.user.age : null),
-    gender: (vm) => (vm.user ? vm.user.gender : null),
-    height: (vm) => (vm.user ? vm.user.height : null),
-    weight: (vm) => (vm.user ? vm.user.weight : null),
-    seriousness: (vm) => (vm.user ? vm.user.seriousness : null)
+    age: {
+      get: (vm) => (vm.user ? vm.user.age : null),
+      set(val) {
+        this.setUserPartial('age', val)
+      }
+    },
+    gender: {
+      get() {
+        const item = this.gendersArray.find((item) => {
+          return item.label === this.user.gender
+        })
+        return item.value
+      },
+      set(val) {
+        const item = this.gendersArray.find((item) => {
+          return item.value === val
+        })
+        this.setUserPartial('gender', item.label)
+      }
+    },
+    height: {
+      get: (vm) => (vm.user ? vm.user.height : null),
+      set(val) {
+        this.setUserPartial('height', val)
+      }
+    },
+    weight: {
+      get: (vm) => (vm.user ? vm.user.weight : null),
+      set(val) {
+        this.setUserPartial('weight', val)
+      }
+    },
+    seriousness: {
+      get() {
+        const item = this.seriousnessArray.find((item) => {
+          return item.label === this.user.seriousness
+        })
+        return item.value
+      },
+      set(val) {
+        const item = this.seriousnessArray.find((item) => {
+          return item.value === val
+        })
+        this.setUserPartial('seriousness', item.label)
+      }
+    },
+    figure: {
+      get() {
+        const item = this.figuresArray.find((item) => {
+          return item.label === this.user.figure
+        })
+        return item.value
+      },
+      set(val) {
+        const item = this.figuresArray.find((item) => {
+          return item.value === val
+        })
+        this.setUserPartial('figure', item.label)
+      }
+    },
+    muscle_mass: {
+      get: (vm) => (vm.user ? vm.user.muscle_mass : null),
+      set(val) {
+        this.setUserPartial('muscle_mass', val)
+      }
+    },
+    body_fat_percentage: {
+      get: (vm) => (vm.user ? vm.user.body_fat_percentage : null),
+      set(val) {
+        this.setUserPartial('body_fat_percentage', val)
+      }
+    }
   },
 
-  mounted() {
-    console.log(this.user)
-    console.log(this.currentUser)
-    if (this.currentUser) {
-      this.user = this.currentUser
+  asyncData({ store }) {
+    const user = store.getters['auth/currentUser']
+    console.log(user)
+    if (!user) return
+
+    return {
+      user
+    }
+  },
+
+  methods: {
+    enableEdit() {
+      this.disabled = false
+    },
+    setUserPartial(key, value) {
+      this.user = { ...this.user, [key]: value }
     }
   }
 }
