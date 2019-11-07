16af15ebc33fe227670c999cc000f94ca847c9d4
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2019-11-07 09:51:22 +0900

    Add validations process

--- frontend/validations/index.js
+++ frontend/validations/index.js
@@ -1,8 +1,40 @@
-export const validateEmail = (validator) => {
+export const validate = (validator, validations) => {
   if (!validator.$dirty) return []
 
   const errors = []
-  !validator.email && errors.push('メールアドレスの形式で入力してください')
-  !validator.required && errors.push('メールアドレスを入力してください')
+  for (const item of validations) {
+    for (const key of Object.keys(item)) {
+      console.log(validations, item, key)
+      !validator[key] && errors.push(item[key])
+    }
+  }
   return errors
+}
+
+export const validateEmail = (validator) => {
+  const validations = [
+    { email: 'メールアドレスの形式で入力してください' },
+    { required: 'メールアドレスを入力してください' }
+  ]
+  return validate(validator, validations)
+}
+
+export const validateNickname = (validator) => {
+  const validations = [
+    { required: 'ニックネームを入力してください' },
+    {
+      maxLength: `${validator.$params.maxLength.max}文字以下で入力してください`
+    }
+  ]
+  return validate(validator, validations)
+}
+
+export const validateDescription = (validator) => {
+  const validations = [
+    { required: '入力が必要です' },
+    {
+      maxLength: `${validator.$params.maxLength.max}文字以下で入力してください`
+    }
+  ]
+  return validate(validator, validations)
 }
