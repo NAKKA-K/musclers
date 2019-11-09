c9cad211cf6473b63cc2fc6f07d3cab20aa78b3f
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2019-11-09 12:33:20 +0900

    Add link to footer

--- frontend/layouts/TheFooter.vue
+++ frontend/layouts/TheFooter.vue
@@ -2,7 +2,15 @@
   <div>
     <v-footer class="font-weight-medium">
       <v-col class="text-center" cols="12">
-        プライバシーポリシー・利用規約・お問い合わせ
+        <nuxt-link to="/privacy_policy" class="mr-4">
+          プライバシーポリシー
+        </nuxt-link>
+        <nuxt-link to="/terms_of_service" class="mr-4">
+          利用規約
+        </nuxt-link>
+        <nuxt-link to="">
+          お問い合わせ
+        </nuxt-link>
       </v-col>
     </v-footer>
   </div>
