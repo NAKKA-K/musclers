2de9fb853d7aa8fdc743e0e0d619c15297fa03c6
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2019-11-14 21:23:57 +0900

    Refactor nest

--- frontend/pages/redirect.vue
+++ frontend/pages/redirect.vue
@@ -9,29 +9,29 @@ export default {
     if (!state.loggedIn) {
       console.error('認証できませんでした')
       return this.$router.push('/auth/login')
-    } else {
-      const postData = {
-        uid: state.user.id,
-        provider: state.strategy,
-        email: state.user.email
-      }
+    }
 
-      try {
-        const res = await this.$axios.$post('/api/auth/sign_in', postData)
-        this.$store.dispatch('auth/setCurrentUser', { user: res.data })
-        if (
-          res.data.email === '' ||
-          res.data.email === null ||
-          typeof res.data.email === 'undefined'
-        ) {
-          this.$router.push('/register')
-        } else {
-          this.$router.push('/home')
-        }
-      } catch (err) {
-        console.error(err)
-        this.$router.push('/auth/login')
+    const postData = {
+      uid: state.user.id,
+      provider: state.strategy,
+      email: state.user.email
+    }
+
+    try {
+      const res = await this.$axios.$post('/api/auth/sign_in', postData)
+      this.$store.dispatch('auth/setCurrentUser', { user: res.data })
+      if (
+        res.data.email === '' ||
+        res.data.email === null ||
+        typeof res.data.email === 'undefined'
+      ) {
+        this.$router.push('/register')
+      } else {
+        this.$router.push('/home')
       }
+    } catch (err) {
+      console.error(err)
+      this.$router.push('/auth/login')
     }
   }
 }
