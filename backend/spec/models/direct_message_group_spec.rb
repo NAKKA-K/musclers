d293e3c62704273bd006b20cb1263324444bd119
kageyasai2 <kd1244160@st.kobedenshi.ac.jp>
2019-11-14 14:28:37 +0900

    [wip]model test

--- backend/spec/models/direct_message_group_spec.rb
+++ backend/spec/models/direct_message_group_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe DirectMessageGroup, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "connect user table" do
+    it "is valid with user_id" do
+      @user1 = create(:user)
+      @user2 = create(:user)
+      direct_message_group = create(:direct_message_group,by_user_id:user1.id,to_user_id:user2.id)
+    end
+  end
 end
