abbb11887537d6973735dbc1cfb351c7fae9e1fc
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2019-11-14 22:17:15 +0900

    Update ui

--- frontend/pages/register.vue
+++ frontend/pages/register.vue
@@ -1,35 +1,35 @@
 <template>
   <v-container>
     <v-col cols="12" sm="6" offset-sm="3">
-      <div class="text-center">
-        <h3 class="margin">メールアドレスの設定</h3>
-        <h5 class="red--text">
-          Facebookでメールアドレスが設定されていませんでした。
-        </h5>
-        <form>
-          <v-text-field
-            v-model="$v.email.$model"
-            :error-messages="emailErrors"
-            label="kobedenshi@gmail.com"
-            prepend-icon="email"
-            single-line
-            required
-            @input="$v.email.$touch()"
-            @blur="$v.email.$touch()"
-          />
-          <v-alert
-            v-if="validErrors.email"
-            border="right"
-            colored-border
-            type="error"
-            elevation="2"
-          >
-            {{ validErrors.email }}
-          </v-alert>
-          <v-btn class="primary" rounded @click="submit">submit</v-btn>
-          <h5 v-if="errMsg" class="red--text">{{ errMsg }}</h5>
-        </form>
-      </div>
+      <h3 class="margin">メールアドレスの設定</h3>
+      <p>
+        Facebookでメールアドレスが設定されていなかったため設定してください。
+      </p>
+      <form>
+        <v-text-field
+          v-model="$v.email.$model"
+          :error-messages="emailErrors"
+          label="hello@example.com"
+          prepend-icon="email"
+          single-line
+          required
+          @input="$v.email.$touch()"
+          @blur="$v.email.$touch()"
+        />
+        <v-alert
+          v-if="validErrors.email"
+          border="right"
+          colored-border
+          type="error"
+          elevation="2"
+        >
+          {{ validErrors.email }}
+        </v-alert>
+
+        <div class="text-center mt-4">
+          <v-btn class="primary" rounded @click="submit">設定する</v-btn>
+        </div>
+      </form>
     </v-col>
   </v-container>
 </template>
