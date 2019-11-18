110cb1a87f6f2e7e1637189defa37860fbef0201
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2019-11-18 15:00:12 +0900

    Add mock routing

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,6 +1,15 @@
 Rails.application.routes.draw do
   root :to => "users#index"
 
+  # HACK: mock
+  namespace :mock, defaults: { format: :json } do
+    scope :api do
+      scope :user do
+        resources :direct_message_groups, only: [:index]
+      end
+    end
+  end
+
   namespace 'api', defaults: { format: :json } do
     namespace 'auth' do
       post '/sign_in', to: 'oauth#facebook'
