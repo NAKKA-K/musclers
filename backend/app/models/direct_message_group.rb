e0a7ef3346e8f6bdb895821ee0a374631fee17d0
kageyasai2 <kd1244160@st.kobedenshi.ac.jp>
2019-11-18 13:58:30 +0900

    [add]association test

--- backend/app/models/direct_message_group.rb
+++ backend/app/models/direct_message_group.rb
@@ -1,5 +1,5 @@
 class DirectMessageGroup < ApplicationRecord
   belongs_to :by_user, class_name: "User", foreign_key: "by_user_id"
   belongs_to :to_user, class_name: "User", foreign_key: "to_user_id"
-  has_many  :direct_messages, :dependent => :destroy
+  has_many   :direct_messages, :dependent => :destroy
 end
--- backend/spec/models/user_spec.rb
+++ backend/spec/models/user_spec.rb
@@ -2,11 +2,13 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   describe "DirectMessageGroup association" do
-    before do
-      let(:user1) { create(:user) }
-      let(:user2) { create(:user) }
+    context "has many by_users" do
+      it { should have_many(:by_users).class_name('DirectMessageGroup').dependent(:destroy) }      
     end
-
+    
+    context "has many to_users" do
+      it { should have_many(:to_users).class_name('DirectMessageGroup').dependent(:destroy) }
+    end
     
   end
 end
