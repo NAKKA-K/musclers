fa24dc30abc0dcb26fe5abd12dd3d42d041a6741
yokotakento <yokoken6164@gmail.com>
2019-11-18 09:54:37 +0900

    correct tags_controller action

--- backend/app/controllers/api/tags_controller.rb
+++ backend/app/controllers/api/tags_controller.rb
@@ -2,6 +2,13 @@ class Api::TagsController < ApplicationController
      #Tagすべての一覧を返すアクション
      def index
         @tags = Tag.all
+        {
+            "status": 200,
+            "message": "タグ一覧を表示しました",
+            "data": {
+                Tag.all(@tags)
+            }
+        }
     end
     
 end
