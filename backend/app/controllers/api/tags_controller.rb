283ee1e31e14e419682b573d6944e288aa08b6c4
yokotakento <yokoken6164@gmail.com>
2019-11-20 13:45:07 +0900

    [追加]ログインの判定とタグを保持していない場合の処理

--- backend/app/controllers/api/tags_controller.rb
+++ backend/app/controllers/api/tags_controller.rb
@@ -11,13 +11,6 @@ class Api::TagsController < ApplicationController
     
     #ユーザーが持っているタグを返す
     def show
-        @user_tags = current_user.user_tags
-        success_res(
-            200, 
-            message: 'ユーザータグ一覧を表示する', 
-            data: @user_tags
-        ) and return
-
         #current_userが空ならエラー
         if current_user.nil?
             error_res(
@@ -26,6 +19,24 @@ class Api::TagsController < ApplicationController
                 err: 'ログインされていません'
             ) and return
         end
+
+        #ユーザーが持っているタグがある、ない場合の処理
+        if current_user.user_tags.nil?
+            error_res(
+                404,
+                message: 'タグが登録されていません'
+                err: 'タグが登録されていません'
+            ) and return
+            
+        else
+            @user_tags = current_user.user_tags
+            success_res(
+                200, 
+                message: 'ユーザータグ一覧を表示する', 
+                data: @user_tags
+            ) and return
+        end
+            
     end
 
 end
