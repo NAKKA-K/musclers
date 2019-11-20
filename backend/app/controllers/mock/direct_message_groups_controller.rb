dc1469aa9ee600e1fc53f7dd25553cbc18fa2444
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2019-11-20 14:29:36 +0900

    Add dmgroup detail api

--- backend/app/controllers/mock/direct_message_groups_controller.rb
+++ backend/app/controllers/mock/direct_message_groups_controller.rb
@@ -32,5 +32,40 @@ module Mock
       ]
       success_res(200, message: '[Mock]: 取得しました', data: data) and return
     end
+
+    def show
+      user = User.all.limit(1).first
+      data = {
+        "id": 1,
+        "by_user": UserSerializer.new(current_user).as_json,
+        "to_user": UserSerializer.new(user).as_json,
+        "created_at": "2019-11-19 04:58:55",
+        "updated_at": "2019-11-19 04:58:55",
+        "direct_messages": [
+          {
+            "id": 1,
+            "body": "Hello, Mr.Muscler!",
+            "send_user_id": user.id,
+            "created_at": "2019-11-19 04:58:55",
+            "updated_at": "2019-11-19 04:58:55"
+          },
+          {
+            "id": 2,
+            "body": "Hello, Mr!",
+            "send_user_id": current_user.id,
+            "created_at": "2019-11-19 04:58:55",
+            "updated_at": "2019-11-19 04:58:55"
+          },
+          {
+            "id": 3,
+            "body": "Let's workout together quadriceps femoris muscle !! 一緒に大腿四頭筋を鍛えましょう！!",
+            "send_user_id": user.id,
+            "created_at": "2019-11-19 04:58:55",
+            "updated_at": "2019-11-19 04:58:55"
+          },
+        ]
+      }
+      success_res(200, message: '[Mock]: 取得しました', data: data) and return
+    end
   end
 end
