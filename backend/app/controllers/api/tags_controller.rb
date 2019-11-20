e35ffcc95629bd6ccd23f2889ea9a1b754700369
yokotakento <yokoken6164@gmail.com>
2019-11-20 11:55:38 +0900

    [修正]jsonの書き直しと非ログイン時のエラー

--- backend/app/controllers/api/tags_controller.rb
+++ backend/app/controllers/api/tags_controller.rb
@@ -12,11 +12,20 @@ class Api::TagsController < ApplicationController
     #ユーザーが持っているタグを返す
     def show
         @user_tags = current_user.user_tags
-        {
-            "status": 200,
-            "message": "ユーザータグ一覧を表示しました",
-            "data": @user_tags
-        }
+        success_res(
+            200, 
+            message: 'ユーザータグ一覧を表示する', 
+            data: @user_tags
+        ) and return
+
+        #current_userが空ならエラー
+        if current_user.nil?
+            error_res(
+                401, 
+                message: 'ログインされていません',
+                err: 'ログインされていません'
+            ) and return
+        end
     end
 
 end
