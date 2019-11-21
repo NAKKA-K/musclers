79d040c462c01cddf5521eadaf990f3d395b6eca
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2019-11-21 22:56:56 +0900

    [WIP] Add group create page

--- frontend/pages/groups/new.vue
+++ frontend/pages/groups/new.vue
@@ -3,3 +3,38 @@
     <h1>グループの新規作成ページ</h1>
   </div>
 </template>
+
+<script>
+export default {
+  middleware: 'auth',
+
+  data: () => ({
+    group: {
+      name: '',
+      description: '',
+      isPublic: true,
+      tags: []
+    }
+  }),
+
+  methods: {
+    sendNewGroup() {
+      const group = {
+        name: this.group.name,
+        description: this.group.description,
+        is_public: this.group.isPublic,
+        tags: this.group.tags
+      }
+      this.$axios
+        .$post('/mock/api/groups', group)
+        .then((res) => {
+          this.$route.push(`/groups/${res.data.id}`)
+        })
+        .catch((err) => {
+          // TODO: 戻ってきたエラーを処理したい
+          console.log(err)
+        })
+    }
+  }
+}
+</script>
