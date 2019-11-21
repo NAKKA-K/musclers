1ad2844d8b682e3d1b182a8e8961b1bf7773acff
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2019-11-21 23:52:55 +0900

    Add group create controller

--- backend/app/controllers/mock/groups_controller.rb
+++ backend/app/controllers/mock/groups_controller.rb
@@ -1,5 +1,6 @@
 module Mock
   class GroupsController < ApplicationController
+    skip_before_action :authenticate_user_from_token!, only: [:index, :show]
 
     def index
       data = []
@@ -69,6 +70,34 @@ module Mock
         "updated_at": "2019-11-19 04:58:55"
       }
       success_res(200, message: '[Mock]: 取得しました', data: data) and return
+    end
+
+    def create
+      data = {
+        name: group_params[:name],
+        description: group_params[:description],
+        is_public: group_params[:is_public],
+        thumbnail: "https://i2.wp.com/dietlife25.com/wp-content/uploads/2019/12/274122b394996dcc766774e82f1bdf0e_m.jpg?resize=1280%2C720&ssl=1",
+        tags: group_params[:tags].map {|id| mock_tags(id) },
+        created_at: "2019-11-19 04:58:55",
+        updated_at: "2019-11-19 04:58:55"
+      }
+      success_res(201, message: '[Mock]: 作成しました', data: data) and return
+    end
+
+    private
+
+    def mock_tag(id)
+      {
+        "id": id,
+        "name": "マッチョ",
+        "created_at": "2019-11-19 04:58:55",
+        "updated_at": "2019-11-19 04:58:55"
+      }
+    end
+
+    def group_params
+      params.permit(:name, :description, :is_public, :tags)
     end
   end
 end
