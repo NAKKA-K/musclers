2a56736a955d50dfc4571f65a07d8a9004127565
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2019-11-21 23:53:31 +0900

    [WIP] Add group new page

--- frontend/pages/groups/new.vue
+++ frontend/pages/groups/new.vue
@@ -1,10 +1,43 @@
 <template>
   <div>
     <h1>グループの新規作成ページ</h1>
+
+    <v-form>
+      <v-text-field
+        v-model="group.name"
+        :error-messages="nameErrors"
+        :counter="128"
+        label="グループ名"
+        required
+        @blur="$v.name.$touch()"
+      >
+      </v-text-field>
+
+      <v-text-field
+        v-model="group.description"
+        :error-messages="descriptionErrors"
+        :counter="2048"
+        label="内容"
+        required
+        @blur="$v.description.$touch()"
+      >
+      </v-text-field>
+
+      <v-switch v-model="group.isPublic" inset label="公開する"></v-switch>
+
+      <v-select
+        v-model="group.tags"
+        :items="tags"
+        label="タグ"
+        multiple
+      ></v-select>
+    </v-form>
   </div>
 </template>
 
 <script>
+import { required, maxLength } from 'vuelidate/lib/validators'
+
 export default {
   middleware: 'auth',
 
@@ -14,8 +47,56 @@ export default {
       description: '',
       isPublic: true,
       tags: []
-    }
+    },
+    tags: null
   }),
+
+  validations: {
+    name: {
+      required,
+      maxLength: maxLength(128)
+    },
+    description: {
+      required,
+      maxLength: maxLength(2048)
+    }
+  },
+
+  computed: {
+    nameErrors() {
+      const errors = []
+      if (!this.$v.name.$dirty) return errors
+      console.log(this.group.name)
+      !this.$v.name.required && errors.push('入力してください')
+      !this.$v.name.maxLength &&
+        errors.push(
+          `${this.$v.name.$params.maxLength.max}文字以内で入力してください`
+        )
+      return errors
+    },
+    descriptionErrors() {
+      const errors = []
+      if (!this.$v.description.$dirty) return errors
+      !this.$v.description.required && errors.push('入力してください')
+      !this.$v.description.maxLength &&
+        errors.push(
+          `${this.$v.description.$params.maxLength.max}文字以内で入力してください`
+        )
+      return errors
+    }
+  },
+
+  async asyncData({ $axios }) {
+    const tags = await $axios
+      .$get('/api/tags')
+      .then((res) => res.data)
+      .catch(() => [])
+    console.log(tags)
+
+    return {
+      tags
+    }
+  },
 
   methods: {
     sendNewGroup() {
