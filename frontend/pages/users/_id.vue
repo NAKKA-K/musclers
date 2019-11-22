eeab90ff969cbc2f9167fc7d4042ca29a8ae8b0a
various3211workd <kd1229568@st.kobedenshi.ac.jp>
2019-11-22 09:25:45 +0900

    [Add] sp site template

--- frontend/pages/users/_id.vue
+++ frontend/pages/users/_id.vue
@@ -1,77 +1,82 @@
 <template>
   <div>
-    <div
-      :style="{ 'background-image': 'url(' + user.thumbnail + ')' }"
-      class="user-information"
-    >
-      <v-col cols="3" offset-md="1">
-        <div class="container bg-rgba">
-          <h2>{{ user.nickname }}</h2>
-          <p>{{ user.description }}</p>
-          <v-btn to="" class="mt-10" outlined color="blue lighten-1">
-            友達申請をする
-          </v-btn>
+    <div class="pc">
+      <div
+        :style="{ 'background-image': 'url(' + user.thumbnail + ')' }"
+        class="user-information"
+      >
+        <v-col cols="3" offset-md="1">
+          <div class="container bg-rgba">
+            <h2>{{ user.nickname }}</h2>
+            <p>{{ user.description }}</p>
+            <v-btn to="" class="mt-10" outlined color="blue lighten-1">
+              友達申請をする
+            </v-btn>
+          </div>
+        </v-col>
+      </div>
+      <div class="services-wrapper">
+        <div class="container">
+          <div class="md-12">
+            <h3>情報</h3>
+          </div>
+          <v-row>
+            <v-col cols="4" offset-md="1">
+              <v-img class="profile-image" :src="user.thumbnail"></v-img>
+            </v-col>
+            <v-col cols="7" class="standard-information">
+              <v-row>
+                <v-col cols="4">
+                  <p>年齢： {{ user.age }}</p>
+                </v-col>
+                <v-col cols="4">
+                  <p>本気度： {{ user.seriousness }}</p>
+                </v-col>
+              </v-row>
+              <v-row>
+                <v-col cols="4">
+                  <p>身長： {{ user.height }}</p>
+                </v-col>
+                <v-col cols="4">
+                  <p>体型： {{ user.figure }}</p>
+                </v-col>
+              </v-row>
+              <v-row>
+                <v-col cols="4">
+                  <p>性別： {{ user.gender }}</p>
+                </v-col>
+                <v-col cols="4">
+                  <p>筋肉量： {{ user.muscle_mass }}</p>
+                </v-col>
+              </v-row>
+              <v-row>
+                <v-col cols="4">
+                  <p>体重： {{ user.wight }}</p>
+                </v-col>
+                <v-col cols="4">
+                  <p>体脂肪率： {{ user.body_fat_percentage }}</p>
+                </v-col>
+              </v-row>
+            </v-col>
+          </v-row>
         </div>
-      </v-col>
-    </div>
-    <div class="services-wrapper">
-      <div class="container">
-        <div class="md-12">
-          <h3>情報</h3>
+      </div>
+      <div v-swiper:mySwiper="swiperOption" class="swiper-container">
+        <ul class="swiper-wrapper">
+          <v-img class="slide-image swiper-slide" :src="user.thumbnail"></v-img>
+          <v-img class="slide-image swiper-slide" :src="user.thumbnail"></v-img>
+          <v-img class="slide-image swiper-slide" :src="user.thumbnail"></v-img>
+          <v-img class="slide-image swiper-slide" :src="user.thumbnail"></v-img>
+          <v-img class="slide-image swiper-slide" :src="user.thumbnail"></v-img>
+        </ul>
+        <div class="outlined">
+          <button class="swiper-button-prev" type="button"></button>
+          <button class="swiper-button-next" type="button"></button>
         </div>
-        <v-row>
-          <v-col cols="4" offset-md="1">
-            <v-img class="profile-image" :src="user.thumbnail"></v-img>
-          </v-col>
-          <v-col cols="7" class="standard-information">
-            <v-row>
-              <v-col cols="4">
-                <p>年齢： {{ user.age }}</p>
-              </v-col>
-              <v-col cols="4">
-                <p>本気度： {{ user.seriousness }}</p>
-              </v-col>
-            </v-row>
-            <v-row>
-              <v-col cols="4">
-                <p>身長： {{ user.height }}</p>
-              </v-col>
-              <v-col cols="4">
-                <p>体型： {{ user.figure }}</p>
-              </v-col>
-            </v-row>
-            <v-row>
-              <v-col cols="4">
-                <p>性別： {{ user.gender }}</p>
-              </v-col>
-              <v-col cols="4">
-                <p>筋肉量： {{ user.muscle_mass }}</p>
-              </v-col>
-            </v-row>
-            <v-row>
-              <v-col cols="4">
-                <p>体重： {{ user.wight }}</p>
-              </v-col>
-              <v-col cols="4">
-                <p>体脂肪率： {{ user.body_fat_percentage }}</p>
-              </v-col>
-            </v-row>
-          </v-col>
-        </v-row>
       </div>
     </div>
-    <div v-swiper:mySwiper="swiperOption" class="swiper-container">
-      <ul class="swiper-wrapper">
-        <v-img class="slide-image swiper-slide" :src="user.thumbnail"></v-img>
-        <v-img class="slide-image swiper-slide" :src="user.thumbnail"></v-img>
-        <v-img class="slide-image swiper-slide" :src="user.thumbnail"></v-img>
-        <v-img class="slide-image swiper-slide" :src="user.thumbnail"></v-img>
-        <v-img class="slide-image swiper-slide" :src="user.thumbnail"></v-img>
-      </ul>
-      <div class="outlined">
-        <button class="swiper-button-prev" type="button"></button>
-        <button class="swiper-button-next" type="button"></button>
-      </div>
+    <div class="sp">
+      <a>test</a>
     </div>
   </div>
 </template>
@@ -195,5 +200,19 @@ export default {
   width: 220px;
   height: 200px;
   border-radius: 10px;
+}
+.pc {
+  display: block !important;
+}
+.sp {
+  display: none !important;
+}
+@media only screen and (max-width: 750px) {
+  .pc {
+    display: none !important;
+  }
+  .sp {
+    display: block !important;
+  }
 }
 </style>
