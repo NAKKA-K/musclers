83470a490a81611c3aa5070f060a0bb14494f715
yokotakento <yokoken6164@gmail.com>
2019-11-25 23:11:22 +0900

    [修正]updateメソッドの修正１

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -13,8 +13,10 @@ class User < ApplicationRecord
             foreign_key: :send_user_id,
             :dependent => :destroy
   has_one_attached :thumbnail
-  validates :email, uniqueness: true, allow_blank: true, format: { with: URI::MailTo::EMAIL_REGEXP }
-  validates :nickname, length: { maximum: 64}
+
+  #モデル側でのNicknameとEmailの正しい値か判定
+  validates :email, uniqueness: true, allow_blank: true, format: { with: URI::MailTo::EMAIL_REGEXP, message: '入力内容が正しくありません'}
+  validates :nickname, length: { maximum: 64, message: 'ニックネームは64文字以内までです。'}
   validates :description, length: { maximum: 1024}
   enum gender:  { none: 0, man: 1, woman: 2, other: 3}, _prefix: true
   enum figure:  {
