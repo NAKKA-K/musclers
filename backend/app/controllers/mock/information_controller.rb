e0dd3f53b6bece73cd2344ab96b756e9bf4d98fd
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2019-11-27 23:25:05 +0900

    Add information mock

--- backend/app/controllers/mock/information_controller.rb
+++ backend/app/controllers/mock/information_controller.rb
@@ -1,9 +1,22 @@
 module Mock
   class InformationController < ApplicationController
     def index
-      @information = Information.where(user_id: current_user.id)
+      data = []
 
-      success_res(200, message: '[Mock]: 取得しました', data: @information) and return
+      10.times.map do |i|
+        {
+          id: i,
+          type: random(4),
+          by_name: '筋肉モンスターたちの集会所',
+          link: '/groups/1',
+          is_read: false,
+          user_id: current_user.id,
+          created_at: "2019-11-19 04:58:55",
+          updated_at: "2019-11-19 04:58:55",
+        }
+      end
+
+      success_res(200, message: '[Mock]: 取得しました', data: data) and return
     end
   end
 end
