35080df56cd6f8455579c7ce8f7848bed414a27e
kageyasai2 <kd1244160@st.kobedenshi.ac.jp>
2019-11-27 22:07:41 +0900

    [change]serializer method

--- backend/app/serializers/latest_message_serializer.rb
+++ backend/app/serializers/latest_message_serializer.rb
@@ -7,19 +7,11 @@ class LatestMessageSerializer < ActiveModel::Serializer
     :updated_at
   )
 
-  def id
-    object.nil? ? "" : object.id
-  end
-
-  def body
-    object.nil? ? "" : object.body
-  end
-
   def created_at
-    object.nil? ? "" : created_date(object.created_at)
+    created_date(object.created_at)
   end
 
   def updated_at
-    object.nil? ? "" : updated_date(object.updated_at)
+    updated_date(object.updated_at)
   end
 end
