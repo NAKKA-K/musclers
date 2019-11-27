9cb7ed057a63758ab2702e910973c54f6e37f32b
kageyasai2 <kd1244160@st.kobedenshi.ac.jp>
2019-11-27 13:39:22 +0900

    [fix]source code

--- backend/app/controllers/api/direct_message_groups_controller.rb
+++ backend/app/controllers/api/direct_message_groups_controller.rb
@@ -1,8 +1,8 @@
 module Api
   class DirectMessageGroupsController < ApplicationController
     include DateHelper
+
     def index
-      user = User.find(current_user.id)
       data = make_dm_list_data
       success_res(200, message: '取得しました', data: data) and return
     end
@@ -21,11 +21,12 @@ module Api
         data
       end
 
+      users_data_hash = fetch_user_data_of_dm_list_hash(current_user_dm_list,current_user.id)
       current_user_dm_list.each do |item|
         latest_message = DirectMessage.where(direct_message_group_id:item.id).order(created_at: "DESC").first
 
-        by_user = User.find(item.by_user_id)
-        to_user = User.find(item.to_user_id)
+        by_user = users_data_hash[item.by_user_id]
+        to_user = users_data_hash[item.to_user_id]
 
         data << {
           "id": item.id,
@@ -38,6 +39,24 @@ module Api
       end
 
       data
+    end
+
+    def fetch_user_data_of_dm_list_hash(dm_list,current_user_id)
+      user_id_list = []
+      dm_list.each do |item|
+        unless user_id_list.include?(item.by_user_id)
+          user_id_list << item.by_user_id
+        end
+
+        unless user_id_list.include?(item.to_user_id)
+          user_id_list << item.to_user_id
+        end
+      end
+
+      users_data = User.where(id: user_id_list).with_attached_thumbnail 
+      hash = users_data.map { |user| [user.id, user] }.to_h
+
+      hash
     end
 
   end
