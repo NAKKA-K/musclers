df4f08d9e3effe5bf9fe8997615c4b2c85c72121
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2019-11-27 23:01:10 +0900

    Add is_read bool to information

--- backend/db/migrate/20191127123511_create_information.rb
+++ backend/db/migrate/20191127123511_create_information.rb
@@ -4,6 +4,7 @@ class CreateInformation < ActiveRecord::Migration[5.2]
       t.integer :type
       t.string :by_name
       t.string :link
+      t.boolean :is_read, default: false, null: false
       t.references :user, foreign_key: true
 
       t.timestamps
--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_27_123511) do
+ActiveRecord::Schema.define(version: 2019_11_27_135225) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -62,6 +62,7 @@ ActiveRecord::Schema.define(version: 2019_11_27_123511) do
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "is_read", default: false, null: false
     t.index ["user_id"], name: "index_information_on_user_id"
   end
 
