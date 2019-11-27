a357c7053efec90528b79e0c59768a7d20e6c976
yokotakento <yokoken6164@gmail.com>
2019-11-27 14:52:18 +0900

    [追加]ブログモデルとテーブルとカラムの追加

---
+++ backend/db/migrate/20191127054633_create_blogs.rb
@@ -0,0 +1,14 @@
+class CreateBlogs < ActiveRecord::Migration[5.2]
+  def change
+    create_table :blogs do |t|
+      #user_idという名前で外部キー制約
+      t.references :user, foreign_key: {to_table: :users}
+      t.string :titile
+      t.text :body
+      t.string :thumbnail
+
+
+      t.timestamps
+    end
+  end
+end
