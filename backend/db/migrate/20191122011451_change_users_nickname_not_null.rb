4ae2babdc20668038bb1ec4fa609fa006679d7c7
kageyasai2 <kd1244160@st.kobedenshi.ac.jp>
2019-11-28 15:15:46 +0900

    [add]change column nickname & set default value

--- backend/db/migrate/20191122011451_change_users_nickname_not_null.rb
+++ backend/db/migrate/20191122011451_change_users_nickname_not_null.rb
@@ -1,5 +1,6 @@
 class ChangeUsersNicknameNotNull < ActiveRecord::Migration[5.2]
   def change
-    change_column_null :users, :nickname, false, default: ''
+    change_column_null :users, :nickname, false
+    change_column :users, :nickname, :string, default: ""
   end
 end
