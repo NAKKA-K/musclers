2c16d44d430efa42704b833c6cd2b7fa05f9cbe0
yokotakento <yokoken6164@gmail.com>
2019-11-28 14:52:22 +0900

    [追加]indexアクションの追加

--- backend/app/controllers/api/blogs_controller.rb
+++ backend/app/controllers/api/blogs_controller.rb
@@ -1,3 +1,13 @@
 class Api::BlogsController < ApplicationController
-    
+    #ブログ一覧を返すアクション
+    #のちにログインしているユーザのブログだけ返すように変更
+    def index
+        @blogs = Blog.all
+        success_res(
+            200,
+            message: 'ブログ一覧をを表示',
+            data: @blogs,
+          ) 
+    end
+
 end
