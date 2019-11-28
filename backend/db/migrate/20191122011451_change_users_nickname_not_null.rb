50902f76ce90d67a675c50c859adfa39d9ff1043
kageyasai2 <kd1244160@st.kobedenshi.ac.jp>
2019-11-28 16:43:43 +0900

    [fix]

--- backend/db/migrate/20191122011451_change_users_nickname_not_null.rb
+++ backend/db/migrate/20191122011451_change_users_nickname_not_null.rb
@@ -1,6 +1,5 @@
 class ChangeUsersNicknameNotNull < ActiveRecord::Migration[5.2]
   def change
-    change_column_null :users, :nickname, false
-    change_column :users, :nickname, :string, default: ""
+    change_column :users, :nickname, :string, null: false, default: ""
   end
 end
