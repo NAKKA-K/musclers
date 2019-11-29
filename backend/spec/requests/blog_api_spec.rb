a5eead4e44fe5706c0ae327f418f5809954d1c2b
yokotakento <yokoken6164@gmail.com>
2019-11-29 11:00:42 +0900

    [追加]blog_api_specにブログ0件の場合の記述

--- backend/spec/requests/blog_api_spec.rb
+++ backend/spec/requests/blog_api_spec.rb
@@ -2,10 +2,17 @@ require 'rails_helper'
 
 describe 'ブログのAPI', type: :request do
   describe 'すべてのブログの一覧表示機能' do
-    context 'ブログが存在している場合' do
-      it 'すべてのブログが表示される' do
-        expect(page).to have_content '新しいブログ'
-      end
+    context 'ブログが1件もない場合' do
+        it 'ブログの表示が0件であることを返す' do
+            expect(page).to have_content '0件のブログ'
+        end
     end
+
+    context 'ブログが存在している場合' do
+        it 'すべてのブログが表示される' do
+            expect(page).to have_content '新しいブログ'
+        end
+    end
+
   end
 end
