4488d841db0c39241aa6ad150fdc876dc789541b
Raccoo <kd1196317@st.kobedenshi.ac.jp>
2019-12-02 09:59:10 +0900

    Rename TheNotification -> TheInformation

---
+++ frontend/pages/TheInformation.vue
@@ -0,0 +1,36 @@
+<!-- 通知 -->
+<template>
+  <v-container>
+    <v-row v-for="info in infos" :key="info.id">
+      <v-col cols="3">
+        <nuxt-link :to="{ name: 'infos-id', params: { id: info.id } }">
+          <v-img
+            class="img-small"
+            :src="info.thumbnail"
+            alt="Avatar"
+            align="middle"
+          />
+        </nuxt-link>
+      </v-col>
+      <v-col cols="9">
+        <nuxt-link :to="{ name: 'infos-id', params: { id: info.id } }">
+          <font size="3"
+            >{{ info.by_name }}から{{ info.type }}が届きました。</font
+          >
+        </nuxt-link>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+<script>
+export default {
+  props: ['infos']
+}
+</script>
+<style>
+.img-small {
+  width: 60px;
+  height: 60px;
+  border-radius: 50%;
+}
+</style>
