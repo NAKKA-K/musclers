15bf9696a09091e348cf0cf1e2ba3af09a43716f
yokotakento <yokoken6164@gmail.com>
2019-12-02 09:54:49 +0900

    [修正]blogのテストの修正

--- backend/spec/requests/blog_api_spec.rb
+++ backend/spec/requests/blog_api_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 describe 'ブログのAPI', type: :request do
   describe 'すべてのブログの一覧表示機能' do
     context 'ブログが1件もない場合' do
+        get api_blogs_path
         blogs = JSON.parse(response.body)
         expect(blogs['message']).to eq 'ブログは投稿されていません'
         expect(blogs['data'].count).to eq 0
@@ -11,13 +12,14 @@ describe 'ブログのAPI', type: :request do
 
     context 'ブログが存在している場合' do
         before do
-            create_list(:blog).each do |blog|
+            create_list().each do |blog|
                 @blogs.create!(blog_id: blog.id)
 
             end
         end
         
         it 'すべてのブログが表示される' do
+            get api_blogs_path
             blogs = JSON.parse(response.body)
             expect(blogs['message']).to eq 'ユーザータグ一覧を表示'
             expect(blogs['data'].count).to_not 0
