09aca89745677a2a7521ac8bafa9c7897f56060a
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2019-12-03 14:04:09 +0900

    Add in development card

--- frontend/pages/auth/deactivate.vue
+++ frontend/pages/auth/deactivate.vue
@@ -1,5 +1,20 @@
 <template>
   <div>
     <h1>退会ページ</h1>
+
+    <v-card class="mx-auto ma-12" max-width="500">
+      <v-card-title class="justify-center">
+        こちらのページは現在開発中です
+      </v-card-title>
+      <v-card-text class="text-center">
+        今しばらくお待ちください。
+      </v-card-text>
+      <v-card-actions class="justify-center">
+        <v-btn text color="primary" to="/home">ホームページに戻る</v-btn>
+        <v-btn text color="primary" @click="() => $router.go(-1)">
+          一つ前のページに戻る
+        </v-btn>
+      </v-card-actions>
+    </v-card>
   </div>
 </template>
