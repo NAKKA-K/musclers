e1cde280c059361d3c71c8b8908ec8423ec3d467
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2019-12-03 14:12:59 +0900

    Move to components

--- frontend/pages/auth/deactivate.vue
+++ frontend/pages/auth/deactivate.vue
@@ -2,19 +2,16 @@
   <div>
     <h1>退会ページ</h1>
 
-    <v-card class="mx-auto ma-12" max-width="500">
-      <v-card-title class="justify-center">
-        こちらのページは現在開発中です
-      </v-card-title>
-      <v-card-text class="text-center">
-        今しばらくお待ちください。
-      </v-card-text>
-      <v-card-actions class="justify-center">
-        <v-btn text color="primary" to="/home">ホームページに戻る</v-btn>
-        <v-btn text color="primary" @click="() => $router.go(-1)">
-          一つ前のページに戻る
-        </v-btn>
-      </v-card-actions>
-    </v-card>
+    <in-development-card></in-development-card>
   </div>
 </template>
+
+<script>
+import InDevelopmentCard from '~/components/organisms/InDevelopmentCard.vue'
+
+export default {
+  components: {
+    InDevelopmentCard
+  }
+}
+</script>
