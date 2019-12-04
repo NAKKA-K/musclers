cce14b68ad74d45158b4b0bf5269072fcbec0160
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2019-12-04 22:30:13 +0900

    Add request example

--- backend/spec/requests/group_spec.rb
+++ backend/spec/requests/group_spec.rb
@@ -2,13 +2,6 @@ require 'rails_helper'
 
 RSpec.describe "Group", type: :request do
   describe "GET /groups" do
-    before do
-      access_token = create(:user).access_token
-      @headers = {
-        'Authorization' => access_token
-      }
-    end
-
     context 'when not created group' do
       it 'return empty data & 200' do
         get api_groups_path
@@ -30,6 +23,23 @@ RSpec.describe "Group", type: :request do
         res = JSON.parse(response.body)
         expect(res['data'].count).to eq 10
       end
+    end
+  end
+
+  describe "GET /groups/:id/join" do
+    context 'when not logged in user' do
+      it 'return status code 401'
+    end
+
+    context 'when logged in user' do
+      before do
+        access_token = create(:user).access_token
+        @headers = {
+          'Authorization' => access_token
+        }
+      end
+
+      it 'return status code 200'
     end
   end
 end
