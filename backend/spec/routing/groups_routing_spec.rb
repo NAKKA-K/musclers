a67e68821c9775992e3dbe2e490fccccec322830
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2019-12-04 22:22:58 +0900

    Add routing spec

--- backend/spec/routing/groups_routing_spec.rb
+++ backend/spec/routing/groups_routing_spec.rb
@@ -5,6 +5,10 @@ RSpec.describe Api::GroupsController, type: :routing do
     it "routes to #index" do
       expect(:get => "/api/groups").to route_to("api/groups#index", format: :json)
     end
+
+    it "routes to #join" do
+      expect(:post => "/api/groups/1/join").to route_to("api/groups#join", id: "1", format: :json)
+    end
   end
 end
 
