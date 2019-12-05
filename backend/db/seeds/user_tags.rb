60619bf17dcd67fde34fafd8355a3fedb143a48c
kageyasai2 <kd1244160@st.kobedenshi.ac.jp>
2019-12-05 15:18:35 +0900

    [change]use Tag.name.values

--- backend/db/seeds/user_tags.rb
+++ backend/db/seeds/user_tags.rb
@@ -1,4 +1,4 @@
-tag_number = [0,1,2,3,4,5,6,7,8,9,10]
+tag_number = Tag.names.values
 User.limit(4).each do |user|
   tag_number.sample(3).each do |number|
     user.user_tags.create(tag_id: number)
