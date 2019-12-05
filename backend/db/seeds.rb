26a78a1cf6677a84873ea061b489947248818cd9
kageyasai2 <kd1244160@st.kobedenshi.ac.jp>
2019-12-05 12:11:49 +0900

    [add]frined test data

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -1,5 +1,5 @@
 # 読み込みたい順番で全`db/seeds/*.rb`のbasenameを配列を定義する
-seeds = %w(users direct_message_groups direct_messages tags groups)
+seeds = %w(users friends direct_message_groups direct_messages tags groups)
 
 Rails.logger.info "Load all seed files 'db/seeds/*.rb'."
 seeds.each do |seed|
