69d65d26841e5a3f65f6350ec69a3b27a94f4922
yokotakento <yokoken6164@gmail.com>
2019-12-09 11:14:17 +0900

    [追加]ブログのcreateメソッドの追加

--- backend/app/controllers/api/blogs_controller.rb
+++ backend/app/controllers/api/blogs_controller.rb
@@ -38,4 +38,36 @@ class Api::BlogsController < ApplicationController
         end
     end
 
+   #ブログの新先登録アクション
+   def create
+        #例外判定
+        begin
+            #Blogオブジェクトの作成時にuser_idを付与する
+            @blog = Blog.new(blog_params.merge(user_id: current_user.id))
+            #正しく作成
+            @blog.save
+            success_res(
+                200,
+                message: 'ブログを新規作成しました',
+                data: @blog,
+            ) and return
+        rescue => e
+            #何かしらのエラー
+            logger.error(e)
+                error_res(
+                    500, 
+                    message: '新規作成に失敗しました',
+                    err: '新規作成に失敗しました',
+                ) and return
+        end
+    end
+
+private
+
+    #送られてきたデータを正しく受け取るためのメソッド
+    def blog_params
+        params.require(:blog).permit(:title, :body)
+    end
+
+
 end
