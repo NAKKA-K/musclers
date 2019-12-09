00bdd2533bf3126b18942a73cffa9ca6688548e3
yokotakento <yokoken6164@gmail.com>
2019-12-09 14:40:29 +0900

    [更新]migrateしてschema.rbを更新

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_05_081949) do
+ActiveRecord::Schema.define(version: 2019_12_09_040521) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -47,8 +47,8 @@ ActiveRecord::Schema.define(version: 2019_12_05_081949) do
 
   create_table "blogs", force: :cascade do |t|
     t.bigint "user_id"
-    t.string "title"
-    t.text "body"
+    t.string "title", default: "", null: false
+    t.text "body", default: "", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_blogs_on_user_id"
