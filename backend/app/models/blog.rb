8da9ffb801236b4d394e5eac74e3ac920c6bae3d
yokotakento <yokoken6164@gmail.com>
2019-12-09 13:37:38 +0900

    [追加]blogモデルに空白、空文字の制約を記述

--- backend/app/models/blog.rb
+++ backend/app/models/blog.rb
@@ -1,4 +1,8 @@
 class Blog < ApplicationRecord
+    #モデルの検証
+    validates :title, presence: true
+    validates :body, presence: true
+
     #usersテーブルに従属しているblogsテーブル
     belongs_to :user
     #blog_tagsに使用されるtag_idの設定
