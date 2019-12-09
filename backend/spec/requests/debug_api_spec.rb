85d1c2e57dd79ed9fb50bb1e6bd9d6402d380385
kageyasai2 <kd1244160@st.kobedenshi.ac.jp>
2019-12-09 21:48:47 +0900

    [add]env test

--- backend/spec/requests/debug_api_spec.rb
+++ backend/spec/requests/debug_api_spec.rb
@@ -12,5 +12,14 @@ describe 'debug login api', type: :request do
         expect(JSON.parse(response.body)['message']).to eq 'デバッグユーザです'
       end
     end
+
+    context 'when rails.env is production' do
+      it 'return status code 401' do
+        allow(Rails).to receive(:env).and_return(ActiveSupport::StringInquirer.new("production"))
+        get api_debug_login_path
+        expect(response).to have_http_status(401)
+        expect(JSON.parse(response.body)['errors'][0]['message']).to eq 'デバッグ環境ではありません'
+      end
+    end
   end
 end
