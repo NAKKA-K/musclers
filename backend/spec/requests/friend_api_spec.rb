ad5fa4d1f127bede55c1481072dbda1f22a00563
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2019-12-10 13:24:03 +0900

    Add test for friend request

---
+++ backend/spec/requests/friend_api_spec.rb
@@ -0,0 +1,39 @@
+require 'rails_helper'
+
+RSpec.describe "Friend", type: :request do
+  describe "POST /user/friends" do
+    context 'when not logged in user' do
+      it 'return status code 401' do
+        post api_friends_path
+        expect(response).to have_http_status(401)
+      end
+    end
+
+    context 'when logged in user' do
+      before do
+        access_token = create(:user).access_token
+        @headers = {
+          'Authorization' => access_token
+        }
+      end
+
+      context 'when not exists the target user' do
+        it 'return status code 404' do
+          post api_friends_path, headers: @headers, params: { user_id: 0 }
+          expect(response).to have_http_status(404)
+        end
+      end
+
+      context 'when not exists the target user' do
+        before do
+          @target = create(:user)
+        end
+
+        it 'return status code 200' do
+          post api_friends_path, headers: @headers, params: { user_id: @target.id }
+          expect(response).to have_http_status(201)
+        end
+      end
+    end
+  end
+end
