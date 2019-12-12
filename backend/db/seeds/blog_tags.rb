5e0d43d186d5b76cd4eaa6ce7fde27b4544d5cda
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2019-12-12 11:48:11 +0900

    Fix blog_tag create

--- backend/db/seeds/blog_tags.rb
+++ backend/db/seeds/blog_tags.rb
@@ -1,8 +1,10 @@
-    tags = Tag.names.values
-    Blog.limit(2).each do |item|
-        tags.sample(3).each do |number|
-            item.blog_tags.create(tag_id: number)
-            p "create blog tag tag_id:#{number}"
-        end
-    end
+tags = Tag.all
 
+ActiveRecord::Base.transaction do
+  Blog.limit(2).each do |blog|
+      tags.sample(3).each do |tag|
+          blog_tag = blog.blog_tags.create!(tag_id: tag.id)
+          p "create blog tag #{blog_tag.id}, tag_id:#{tag.id}"
+      end
+  end
+end
