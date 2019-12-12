ac5439413419a11f9b16c97301d17942c5161af1
kageyasai2 <kd1244160@st.kobedenshi.ac.jp>
2019-12-12 16:05:27 +0900

    [add]thumbnail url

--- backend/app/serializers/information_serializer.rb
+++ backend/app/serializers/information_serializer.rb
@@ -1,7 +1,17 @@
 class InformationSerializer < ActiveModel::Serializer
-  attributes :id, :genre, :by_name, :link
+  include Rails.application.routes.url_helpers
+  attributes :id, :genre, :by_name, :link, :thumbnail
 
   def genre
     object.genre_i18n
+  end
+
+  def thumbnail
+    user = User.find_by_nickname(object.by_name)
+    if user.thumbnail.attached?
+      url_for(user.thumbnail)
+    else
+      root_url + "images/noimage.png"
+    end
   end
 end
