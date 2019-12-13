0e3f3004e7a40b0ef66abbab3e84a285018ba19d
various3211workd <43775946+various3211workd@users.noreply.github.com>
2019-12-13 17:54:46 +0900

    Update home.vue

--- frontend/pages/home.vue
+++ frontend/pages/home.vue
@@ -36,7 +36,7 @@
       <div slot="button-prev" class="swiper-button-prev"></div>
       <div slot="button-next" class="swiper-button-next"></div>
     </div>
-    <nuxt-link to="/groups" class="d-inline-block search-text subtitle-1">
+    <nuxt-link to="/user_blogs" class="d-inline-block search-text subtitle-1">
       ユーザーブログをもっと見る
       <v-icon class="blue--text">chevron_right</v-icon>
     </nuxt-link>
