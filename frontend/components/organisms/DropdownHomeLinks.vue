7fd7a058ae7e48ee66c24cbcfcd292667597b843
various3211workd <kd1229568@st.kobedenshi.ac.jp>
2019-12-21 11:41:35 +0900

    [Change] responsive design header icons

--- frontend/components/organisms/DropdownHomeLinks.vue
+++ frontend/components/organisms/DropdownHomeLinks.vue
@@ -1,62 +1,41 @@
 <template>
-  <v-toolbar-items>
-    <!--
-    <v-btn to="/home" text>
-      <v-icon left>home</v-icon>
-      ホーム
-    </v-btn>
-    <v-btn to="/groups" text>
-      <v-icon left>groups</v-icon>
-      グループ
-    </v-btn>
-    <v-btn to="/user_blogs" text>
-      <v-icon left>menu_book</v-icon>
-      ユーザーブログ
-    </v-btn>
-    -->
-    <!--
-    <v-list>
-      <v-list-item v-for="(item, i) in items" :key="i" :to="item.link">
-        <v-avatar size="25px" class="mr-2">
-          <v-icon v-text="item.icon"></v-icon>
-        </v-avatar>
-        <v-list-item-content>
-          <v-list-item-title v-text="item.title"></v-list-item-title>
-        </v-list-item-content>
-      </v-list-item>
-    </v-list>
-    -->
+  <div>
+    <div class="hidden-md-and-down hidden-xl-only">
+      <v-toolbar-items>
+        <v-btn v-for="(item, i) in items" :key="i" :to="item.link" text>
+          <v-icon left v-text="item.icon"></v-icon>
+          {{ item.title }}
+        </v-btn>
+      </v-toolbar-items>
+    </div>
+    <div class="hidden-sm-and-up">
+      <v-menu class="hidden-sm-and-up" bottom>
+        <template v-slot:activator="{ on }">
+          <v-layout
+            class="ml-3 header__item"
+            align-center="align-center"
+            v-on="on"
+          >
+            <span>Home</span>
+            <v-icon class="header__item__icon--drop" size="18px" light="light">
+              arrow_drop_down
+            </v-icon>
+          </v-layout>
+        </template>
 
-    <v-btn v-for="(item, i) in items" :key="i" :to="item.link" text>
-      <v-icon left v-text="item.icon"></v-icon>
-      {{ item.title }}
-    </v-btn>
-  </v-toolbar-items>
-  <!--
-  <v-menu bottom>
-    <template v-slot:activator="{ on }">
-      <v-layout class="ml-3 header__item" align-center="align-center" v-on="on">
-        <span>Home</span>
-        <v-icon class="header__item__icon--drop" size="18px" light="light">
-          arrow_drop_down
-        </v-icon>
-      </v-layout>
-    </template>
-    -->
-  <!-- TODO: 全員がアクセスできるまとめページ的なのにリンクする -->
-  <!--
-    <v-list>
-      <v-list-item v-for="(item, i) in items" :key="i" :to="item.link">
-        <v-avatar size="25px" class="mr-2">
-          <v-icon v-text="item.icon"></v-icon>
-        </v-avatar>
-        <v-list-item-content>
-          <v-list-item-title v-text="item.title"></v-list-item-title>
-        </v-list-item-content>
-      </v-list-item>
-    </v-list>
-  </v-menu>
-  -->
+        <v-list>
+          <v-list-item v-for="(item, i) in items" :key="i" :to="item.link">
+            <v-avatar size="25px" class="mr-2">
+              <v-icon v-text="item.icon"></v-icon>
+            </v-avatar>
+            <v-list-item-content>
+              <v-list-item-title v-text="item.title"></v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list>
+      </v-menu>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -70,3 +49,40 @@ export default {
   })
 }
 </script>
+<!--
+<template>
+  <v-menu bottom>
+    <template v-slot:activator="{ on }">
+      <v-layout class="ml-3 header__item" align-center="align-center" v-on="on">
+        <span>Home</span>
+        <v-icon class="header__item__icon--drop" size="18px" light="light">
+          arrow_drop_down
+        </v-icon>
+      </v-layout>
+    </template>
+
+    <v-list>
+      <v-list-item v-for="(item, i) in items" :key="i" :to="item.link">
+        <v-avatar size="25px" class="mr-2">
+          <v-icon v-text="item.icon"></v-icon>
+        </v-avatar>
+        <v-list-item-content>
+          <v-list-item-title v-text="item.title"></v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
+    </v-list>
+  </v-menu>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    items: [
+      { link: '/home', icon: 'home', title: 'ホーム' },
+      { link: '/groups', icon: 'group', title: 'グループ' },
+      { link: '/user_blogs', icon: 'menu_book', title: 'ユーザーブログ' }
+    ]
+  })
+}
+</script>
+-->
