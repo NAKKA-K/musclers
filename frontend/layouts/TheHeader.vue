35aa9d7c302066cea41d2246da65b0eb3d9f864b
various3211workd <kd1229568@st.kobedenshi.ac.jp>
2019-12-22 16:16:26 +0900

    [Change] header navigation bar

--- frontend/layouts/TheHeader.vue
+++ frontend/layouts/TheHeader.vue
@@ -1,37 +1,72 @@
 <template>
   <div>
-    <v-toolbar flat :color="'#ffac12'">
+    <v-navigation-drawer v-model="drawer" app clipped>
+      <v-list dense>
+        <v-list-item v-for="(item, i) in items" :key="i" :to="item.link">
+          <v-list-item-action>
+            <v-avatar size="25px" class="mr-2">
+              <v-icon v-text="item.icon"></v-icon>
+            </v-avatar>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title v-text="item.title"></v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-app-bar app clipped-left flat :color="'#ffac12'">
+      <v-app-bar-nav-icon
+        class="hidden-sm-and-up"
+        @click.stop="drawer = !drawer"
+      />
       <v-toolbar-title>
         <nuxt-link v-if="currentUser" to="/auth/mypage" class="header-title">
           Muscler's
         </nuxt-link>
         <nuxt-link v-else to="/" class="header-title">Muscler's</nuxt-link>
       </v-toolbar-title>
-      <dropdown-home-links></dropdown-home-links>
-
+      <v-toolbar-items>
+        <v-btn
+          v-for="(item, i) in items"
+          :key="i"
+          :to="item.link"
+          text
+          class="hidden-md-and-down hidden-xl-only"
+        >
+          <v-icon left v-text="item.icon"></v-icon>
+          {{ item.title }}
+        </v-btn>
+      </v-toolbar-items>
       <v-spacer></v-spacer>
-
       <the-right-side-or-the-header></the-right-side-or-the-header>
-    </v-toolbar>
+    </v-app-bar>
   </div>
 </template>
 
 <script>
 import { mapGetters } from 'vuex'
 import TheRightSideOrTheHeader from '~/components/organisms/TheRightSideOrTheHeader.vue'
-import DropdownHomeLinks from '~/components/organisms/DropdownHomeLinks.vue'
+// import DropdownHomeLinks from '~/components/organisms/DropdownHomeLinks.vue'
 
 export default {
   components: {
-    TheRightSideOrTheHeader,
-    DropdownHomeLinks
+    TheRightSideOrTheHeader
+    // DropdownHomeLinks
   },
-
   computed: {
     ...mapGetters({
       currentUser: 'auth/currentUser'
     })
-  }
+  },
+  data: () => ({
+    drawer: false,
+    items: [
+      { link: '/home', icon: 'home', title: 'ホーム' },
+      { link: '/groups', icon: 'group', title: 'グループ' },
+      { link: '/user_blogs', icon: 'menu_book', title: 'ユーザーブログ' }
+    ]
+  })
 }
 </script>
 
