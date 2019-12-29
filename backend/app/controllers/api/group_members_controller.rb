8f1a245ffd7d9495593b8d40d475d8a8c8e6df3c
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2019-12-29 21:24:21 +0900

    Add exception handling

--- backend/app/controllers/api/group_members_controller.rb
+++ backend/app/controllers/api/group_members_controller.rb
@@ -2,7 +2,16 @@ class Api::GroupMembersController < ApplicationController
   skip_before_action :authenticate_user_from_token!, only: [:show]
 
   def show
-    group_members = Group.preload(:users).find(params[:group_id])
+    begin
+      group_members = Group.preload(:users).find(params[:group_id])
+    rescue ActiveRecord::RecordNotFound
+      error_res(
+          404,
+          message: '指定したグループは存在しません',
+          err: '指定したグループは存在しません',
+      ) and return
+    end
+
     data = GroupMemberSerializer.new(group_members).as_json
     success_res(200, message: 'グループメンバーが見つかりました', data: data) and return
   end
