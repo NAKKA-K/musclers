6e2fc409fce72e8c298f8a83f1eae1069434477c
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2019-12-31 22:15:19 +0900

    Add spec for group validates

--- backend/spec/models/group_spec.rb
+++ backend/spec/models/group_spec.rb
@@ -18,4 +18,24 @@ RSpec.describe Group, type: :model do
       it { should have_many(:group_messages).dependent(:destroy) }
     end
   end
+
+  describe "group validations" do
+    it "is invalid without a name" do
+      group = Group.new(name: nil)
+      group.valid?
+      expect(group.errors[:name].present?).to be_truthy
+    end
+
+    it "is invalid without a description" do
+      group = Group.new(description: nil)
+      group.valid?
+      expect(group.errors[:description].present?).to be_truthy
+    end
+    it "is invalid without a is_public" do
+      group = Group.new(is_public: nil)
+      group.valid?
+      expect(group.errors[:is_public].present?).to be_truthy
+    end
+
+  end
 end
