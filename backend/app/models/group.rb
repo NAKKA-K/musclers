dfd306c5c22cf5d151c2aecf29fa08e83ffe9ac3
NAKKA-K <kd1208740@st.kobedenshi.ac.jp>
2019-12-31 22:03:49 +0900

    Add group validation

--- backend/app/models/group.rb
+++ backend/app/models/group.rb
@@ -3,4 +3,8 @@ class Group < ApplicationRecord
   has_many :users,through: :group_users, :dependent => :destroy
   has_many :group_messages, :dependent => :destroy
   has_one_attached :thumbnail
+
+  validates :name, presence: true, length: { maximum: 128 }
+  validates :description, presence: true, length: { maximum: 4096 }
+  validates :is_public, presence: true
 end
