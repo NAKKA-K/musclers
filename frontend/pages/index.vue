6455b8a188b3c75a5c98a59274d103b4c944d395
kageyasai2 <kd1244160@st.kobedenshi.ac.jp>
2020-01-15 12:26:56 +0900

    [fix]LP design

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -2,9 +2,14 @@
   <div>
     <div class="top-wrapper">
       <div class="container">
-        <h1>筋肉を今よりも素敵に。</h1>
-        <p>筋肉大好き人間が集まるマッチングアプリです。</p>
-        <p>他人が投稿した筋肉を見たり、筋トレをしましょう。</p>
+        <h1>筋トレであなたの</h1>
+        <h1>人生を笑顔に</h1>
+        <p>
+          筋トレ初心者から上級者の方までご利用できるマッチングアプリです。
+        </p>
+        <p>
+          Muscler'sで友達を作り筋トレライフを充実させましょう！！
+        </p>
         <div class="register-wrapper mt-5">
           <v-btn to="/auth/login" class="register-btn" dark nuxt>
             Muscler'sを始める
@@ -20,7 +25,7 @@
         <div class="services">
           <div class="service-box">
             <div class="service-icon">
-              <v-icon size="90">emoji_people</v-icon>
+              <v-icon size="120">emoji_people</v-icon>
               <p>マッチング</p>
             </div>
             <p class="service-text">
@@ -29,7 +34,7 @@
           </div>
           <div class="service-box">
             <div class="service-icon">
-              <v-icon size="90">person_add</v-icon>
+              <v-icon size="120">person_add</v-icon>
               <p>友達申請</p>
             </div>
             <p class="service-text">
@@ -38,7 +43,7 @@
           </div>
           <div class="service-box">
             <div class="service-icon">
-              <v-icon size="90">message</v-icon>
+              <v-icon size="120">message</v-icon>
               <p>メッセージ</p>
             </div>
             <p class="service-text">
@@ -47,7 +52,7 @@
           </div>
           <div class="service-box">
             <div class="service-icon">
-              <v-icon size="90">search</v-icon>
+              <v-icon size="120">search</v-icon>
               <p>検索</p>
             </div>
             <p class="service-text">
@@ -56,7 +61,7 @@
           </div>
           <div class="service-box">
             <div class="service-icon">
-              <v-icon size="90">group</v-icon>
+              <v-icon size="120">group</v-icon>
               <p>グループ</p>
             </div>
             <p class="service-text">
@@ -65,7 +70,7 @@
           </div>
           <div class="service-box">
             <div class="service-icon">
-              <v-icon size="90">fitness_center</v-icon>
+              <v-icon size="120">fitness_center</v-icon>
               <p>筋肉</p>
             </div>
             <p class="service-text">
