613e36cb5148d1131cf9319e7ba87f4d307c0bd6
kageyasai2 <kd1244160@st.kobedenshi.ac.jp>
2020-02-05 18:25:57 +0900

    [change]origin

--- backend/config/initializers/cors.rb
+++ backend/config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:3000','https://musclers.net'
+    origins '*'
 
     resource '*',
       headers: :any,
